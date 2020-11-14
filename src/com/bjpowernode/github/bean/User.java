61f938c94b10f9e2dfe60b915ccf8864a65ee003
liqiang <1020372814@qq.com>
2020-11-14 16:50:57 +0800

    第一次提交项目

---
+++ src/com/bjpowernode/github/bean/User.java
@@ -0,0 +1,20 @@
+package com.bjpowernode.github.bean;
+
+/**
+ * @ProjectName: githubDemo
+ * @Package: com.bjpowernode.github.bean
+ * @Description: java类作用描述
+ * @Author: 李强
+ * @CreateDate: 2020/11/14 0014 16:45
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class User {
+
+    private String name;
+    private int age;
+
+
+}
+
